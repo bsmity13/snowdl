e6ad5246db2e4a09c76f2eff0aab009719aa7450
bsmity13 <bsmity13@gmail.com>
2021-02-23 11:53:22 -0700

    Updated mosaic_daymet().

--- R/Daymet_fun.R
+++ R/Daymet_fun.R
@@ -78,8 +78,45 @@ get_daymet_swe <- function(year,
 #                tile = c(12095, 12096),
 #                out_dir = "data")
 
+#' Mosaic Daymet files
+#'
+#' Mosaic daymet tiles by year and day
+#'
+#' @param dir `[character]` Directory where Daymet data are stored
+#' @param days `[integer = 1:365]` Integer(ish) vector giving the days you want
+#' retained in the data. Defaults to all days (1:365). See note in Details.
+#' @param out_dir `[character = "data"]` Directory to save mosaicked output.
+#' @param format `[character = "raster"]` Output format of raster. Either
+#' `"raster"` (.grd) or `"GTiff"` (.tif).
+#' @param extent `[Extent = NULL]` Optional. Extent for cropping SNODAS rasters.
+#' If `NULL` (default), no cropping occurs.
+#' @param verbose `[logical = TRUE]` Determines whether the user will be
+#' notified of progress.
+#'
+#' @details This function takes the tiles downloaded by
+#' \code{\link{get_daymet_swe}()} and mosaics them together by year and day.
+#' Each Daymet NetCDF file has 365 bands corresponding to days of the year. This
+#' function finds all the `*.nc` files for each year in the folder, then
+#' determines which bands (days) to load with the argument `days`, so
+#' processing is sped up noticeably if you only specify the days you need. The
+#' output is then saved in a single-band GeoTIFF labeled with yyyy_day in the
+#' filename.
+#'
+#' A note on Daymet days. During leap years, there are still only 365 bands in
+#' the raster. December 31st is omitted during leap years.
 #' @export
-mosaic_daymet <- function(dir, out_dir = "."){
+mosaic_daymet <- function(dir,
+                          days = 1:365,
+                          out_dir = ".",
+                          format = c("raster", "GTiff"),
+                          extent = NULL,
+                          verbose = TRUE){
+
+  # Check if out_dir needs to be created
+  if (!dir.exists(out_dir)) {
+    dir.create(out_dir, recursive = TRUE)
+  }
+
   # Files
   f <- list.files(dir, pattern = ".nc", full.names = TRUE)
   # Basenames
@@ -87,20 +124,47 @@ mosaic_daymet <- function(dir, out_dir = "."){
   # Get years
   years <- unique(unlist(lapply(strsplit(bn, split = "_", fixed = TRUE),
                   getElement, 2)))
+  # Initialize character vector to store filenames for return
+  file_names <- character()
   # Mosaic each year
   for (y in years){
     # Files for that year
     fy <- grep(y, f, value = TRUE)
-    # Load all
-    rl <- lapply(fy, raster::raster)
-    # Add mosaic arguments
-    rl$fun <- mean
-    rl$na.rm <- TRUE
-    # Mosaic
-    m <- do.call(raster::mosaic, rl)
-    # Save
-    raster::writeRaster(m, file.path(out_dir, paste0(y, "_swe.grd")))
+    # Status
+    if (verbose) {
+      cat(paste0("Year: ", y, "\n"))
+    }
+    # Days requested
+    for (d in days) {
+      # Status
+      if (verbose) {
+        cat(paste0(" ... Day: ", d, "\n"))
+      }
+      # Load all
+      rl <- lapply(fy, raster::raster, band = d)
+      # Add mosaic arguments
+      rl$fun <- mean #Shouldn't matter b/c tiles do not overlap
+      rl$na.rm <- TRUE
+      # Mosaic
+      m <- do.call(raster::mosaic, rl)
+      # Crop or reproject
+      if (!is.null(extent)) {
+        r <- raster::crop(m, extent, snap = "out")
+      }
+      # Pad day with 0s for filename
+      day <- formatC(d, width = 3, format = "d", flag = "0")
+      # Decide extension
+      ext <- switch(format[1],
+                    raster = ".grd",
+                    GTiff = ".tif")
+      # Create filename
+      fn <- file.path(out_dir, paste0(y, "_", day, "_swe", ext))
+      # Save
+      raster::writeRaster(m, fn, format = format[1], overwrite = TRUE)
+      # Append fn to return later
+      file_names <- c(file_names, fn)
+    }
   }
 
-  return(file.path(out_dir, paste0(years, "_swe.grd")))
+  return(file_names)
 }
